3ee16cfc7c01dc2a03ddc8fea0b163c0cd3fcc21
unicod3 <unicod3@hotmail.com>
2015-06-21 06:22:14 +0300

    initial commit

---
+++ app/src/main/java/com/runba/unicod3/barcodemanager/MainActivity.java
@@ -0,0 +1,77 @@
+package com.runba.unicod3.barcodemanager;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+
+public class MainActivity extends ActionBarActivity {
+    Button btnScan;
+    TextView txtResult;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnScan = (Button)findViewById(R.id.btnScan);
+
+        btnScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                IntentIntegrator integrator = new IntentIntegrator(MainActivity.this);
+                integrator.initiateScan();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        txtResult = (TextView)findViewById(R.id.txtResult);
+        if(result != null) {
+            if(result.getContents() == null) {
+                //Log.d("MainActivity", "Cancelled scan");
+                txtResult.setText(R.string.cancelled);
+            } else {
+               // Log.d("MainActivity", "Scanned");
+                txtResult.setText( R.string.scanTitle + " \n" + result.getContents());
+            }
+        } else {
+            Log.d("MainActivity", "Weird");
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+
+}
